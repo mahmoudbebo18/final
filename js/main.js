817f369a19f3cd552a60b49a7a949cf3074c51f0
mahmoudelkhateb <bebobest2014@gmail.com>
2021-06-09 22:08:57 +0200

    finish task

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+
+    $(".owl-container .owl-carousel").owlCarousel({
+        items: 6,
+        rtl: true,
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        mouseDrag: true,
+        touchDrag: true,
+        autoplayTimeout: 1500,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            425: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 4,
+            },
+        },
+    });
+})
+
+function increase() {
+    var a = 1;
+    var textBox = document.getElementById("text");
+    textBox.value++;
+
+}
+
+function decrease() {
+    var textBox = document.getElementById("text");
+    textBox.value--;
+}
+
+$(document).ready(function() {
+    var $textbox = $("#incdec input");
+
+    $("#up").click(function() {
+        var value = parseInt($textbox.val(), 10);
+
+        $textbox.val(value + 1);
+    });
+
+    $("#down").click(function() {
+        var value = parseInt($textbox.val(), 10);
+
+        if (value > 0) {
+            $textbox.val(value - 1);
+        }
+    });
+    /*
+        $('.add-to-order').click(function() {
+            $('.custom-your-meal').fadeOut().next().fadeIn()
+        })*/
+});
